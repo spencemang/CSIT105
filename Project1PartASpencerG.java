47bca3fde88f094201f8fc71267995e82c3c3799
spencemang <96602262+spencemang@users.noreply.github.com>
2025-02-20 23:32:06 -0500

    Update Project1PartASpencerG.java

--- Project1PartASpencerG.java
+++ Project1PartASpencerG.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 /**
  *
- * @author spencer Granger
+ * @author Spencer Granger
  */
 public class Project1PartASpencerG {
     public static void main(String[] args) {
@@ -15,29 +15,32 @@ public class Project1PartASpencerG {
         int totalServings;
         int servingCals;
         int cookiesEaten;
-        double cookiesperServing;
-        double servingsEaten;
-        double totalCals;
-        double calsperCookie;
+        double eatenCals;
+        double cookiesPerServing;
+        double calsPerCookie;
         
-        System.out.print("Which cookies did you eat?");
+        System.out.print("Which cookies did you eat? ");
         cookieType=input.nextLine();
         
-        System.out.print("How many cookies are there in a bag?");
+        System.out.print("How many cookies are there in a bag? ");
         cookiesinBag=input.nextInt();
         
-        System.out.print("\nHow many servings are there in a bag?");
+        System.out.print("How many servings are there in a bag? ");
         totalServings=input.nextInt();
         
-        System.out.print("\nHow many calories are in a serving?");
+        System.out.print("How many calories are in a serving? ");
         servingCals=input.nextInt();
         
-        System.out.print("\n How many cookies did you eat?");
+        System.out.print("\n How many cookies did you eat? ");
         cookiesEaten=input.nextInt();
         
-         cookiesperServing=(double)cookiesinBag/totalServings;
-         eatenCals= cookiesperServing
-
+         cookiesPerServing=(double)cookiesinBag/totalServings;
+        
+         calsPerCookie=servingCals/cookiesPerServing;
+         
+         eatenCals=calsPerCookie*cookiesEaten;
+         
+         System.out.printf( "\n%d %s cookies is %.1f calories\n", cookiesEaten, cookieType, eatenCals);
 
 
 
