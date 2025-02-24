b96b039cf633663bda521b639b24fda61a78144f
Spencer Granger <spencergranger@corban.edu>
2025-02-23 23:11:46 -0500

    Began Project 1B

---
+++ Project1PartBSpencerG/src/project1partbspencerg/Project1PartBSpencerG.java
@@ -0,0 +1,23 @@
+
+package project1partbspencerg;
+
+/**
+ *
+ * @author Spencer Granger
+ */
+public class Project1PartBSpencerG {
+
+    public static void main(String[] args) {
+        
+        double cupsOfSugar=1.5/48;
+        double cupsOfButter=1/48;
+        double cupsOfFlour=2.75/48;
+        
+        System.out.println(cupsOfSugar);
+        System.out.println(cupsOfButter);
+        System.out.println(cupsOfFlour);
+        
+      
+    }
+    
+}
